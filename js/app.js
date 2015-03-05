7fe3fe26a24d6dd3f310021c5ad16fd3b65dd580
Cam Gillespie <gillespie.cam@gmail.com>
2015-03-05 14:57:47 -0700

    Added increment func to ViewModel, updated databind in HTML

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var ViewModel = function (){
   this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
   this.imgAttribution = ko.observable('some guy');
 
-  this.incrementCounter = function () {}
+  this.incrementCounter = function () {
+    this.clickCount(this.clickCount() + 1);
+  }
 }
 
 ko.applyBindings(new ViewModel());
